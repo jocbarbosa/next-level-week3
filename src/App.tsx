2453f30476e29ac456130e1f59c47b3164dc136c
Joao C Barbosa <joaao@outlook.com>
2020-10-14 10:54:05 -0300

    feat: create landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { FaSignInAlt } from 'react-icons/fa';
+
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+import logoImg from './images/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças</p>
+        </main>
+
+        <div className="location">
+          <strong>Ceará</strong>
+          <span>Fortaleza</span>
+        </div>
+
+        <a href="#" className="enter-app">
+          <FaSignInAlt size={20} color="#4f4f4f" />
+        </a>
+      </div>
+
     </div>
   );
 }
