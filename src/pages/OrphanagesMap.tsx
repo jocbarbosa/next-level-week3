2c2f8fae984132acc32bab0848891382968f32f2
Joao C Barbosa <joaao@outlook.com>
2020-10-14 12:53:57 -0300

    feat: create orphanage map page

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaPlus } from 'react-icons/fa';
+
+import mapMarker from '../images/marker.svg';
+
+import '../styles/pages/orphanage-map.css';
 
 function OrphanagesMap() {
     return (
-        <h1>Orphanages page</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarker} alt="Happy" />
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita</p>
+                </header>
+
+                <footer>
+                    <strong>Fortaleza</strong>
+                    <span>Ceará</span>
+                </footer>
+            </aside>
+
+            <div></div>
+
+            <Link className="create-orphanage" to="#">
+                <FaPlus size={32} color="#FFF" />
+            </Link>
+        </div>
     );
 }
 
