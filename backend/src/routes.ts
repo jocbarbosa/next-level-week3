2dafe33addc2a214cd1db80fbd6f5faa9151709b
Joao C Barbosa <joaao@outlook.com>
2020-10-19 20:06:19 -0300

    feat: configure multer and orphanage upload

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,13 @@
 import express, { Router } from 'express';
+import multer from 'multer';
 import OrphanagesController from './controllers/OrphanagesController';
 
-const router = Router();
+import uploadConfig from '../src/config/upload';
 
-router.post('/orphanages', OrphanagesController.create);
+const router = Router();
+const upload = multer(uploadConfig);
+
+router.post('/orphanages', upload.array('images'), OrphanagesController.create);
 router.get('/orphanages', OrphanagesController.index);
 router.get('/orphanages/:id', OrphanagesController.show);
 
