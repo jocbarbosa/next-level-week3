f02fae6956a84d74c7a9e80270af4f87a883e157
Joao C Barbosa <joaao@outlook.com>
2020-10-20 19:14:11 -0300

    feat: enable CORS for all origins

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 import errorHandler from './helpers/errors/handler';
@@ -8,6 +9,7 @@ import errorHandler from './helpers/errors/handler';
 import './database/connection';
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
