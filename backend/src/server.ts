29d8bb4bba6051d49703c6fd50a75463733a4dc4
Joao C Barbosa <joaao@outlook.com>
2020-10-20 17:03:46 -0300

    feat: add an error handler as an express middleware

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import path from 'path';
+import 'express-async-errors';
+
 import routes from './routes';
+import errorHandler from './helpers/errors/handler';
 
 import './database/connection';
 const app = express();
@@ -8,8 +11,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
-
-
-
+app.use(errorHandler);
 
 app.listen(process.env.PORT || 3333);
